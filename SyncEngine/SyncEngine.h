f6c7970713fa778660296dc67782f54e31b80c51
qujinshan <qujinshan@hujiang.com>
2018-06-06 15:51:34 +0800

    Initial Commit

---
+++ SyncEngine/SyncEngine.h
@@ -0,0 +1,19 @@
+//
+//  SyncEngine.h
+//  SyncEngine
+//
+//  Created by Purkylin King on 2018/6/6.
+//  Copyright © 2018年 Purkylin King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SyncEngine.
+FOUNDATION_EXPORT double SyncEngineVersionNumber;
+
+//! Project version string for SyncEngine.
+FOUNDATION_EXPORT const unsigned char SyncEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyncEngine/PublicHeader.h>
+
+
